91e141043ca6322532d0f0439c2cfb6d35e76f8e
TENX-S <ttenx@pm.me>
2020-06-24 22:21:09 +0800

    simplify the code, add comment

--- generate_random_password/src/main.rs
+++ generate_random_password/src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use std::env;
 use rand::Rng;
 use rand::prelude::*;
@@ -9,9 +7,9 @@ const GEN: fn(u8, u8) -> Vec<String> = |start, end| {
                      .iter()
                      .map(|asc_num| (*asc_num as char).to_string())
                      .collect::<Vec<String>>()
-};
+}; // return the ASCII characters in Vec we need
 
-const RAND_IDX: fn(usize, usize) -> Vec<usize> = |n, cnt| {
+const RAND_IDX: fn(usize, usize) -> Vec<usize> = |n, cnt| { // Given the amount and upper bound, generate the random index
     let mut rng = rand::thread_rng();
     let mut idx;
     let mut idxs = vec![];
@@ -24,36 +22,38 @@ const RAND_IDX: fn(usize, usize) -> Vec<usize> = |n, cnt| {
 
 fn main() {
 
-    let mut length: usize = 16;
-    let mut symbol: usize = 4;
-    let mut number: usize = 4;
+    let length; // length of the password
+    let symbol; // amount of the symbols
+    let number; // amount of the numbers
 
-    let mut symbols_buffer = GEN(33, 47);
-    symbols_buffer.append(&mut GEN(58, 64));
-    symbols_buffer.append(&mut GEN(91, 96));
-    symbols_buffer.append(&mut GEN(123, 126));
+    let symbols =
+        vec![GEN(33, 47), GEN(58, 64), GEN(91, 96), GEN(123, 126)]
+            .iter_mut()
+            .fold(vec![], |mut acc, x| {acc.append(x); acc});
 
-    let mut letters_buffer = GEN(65, 90);
-    letters_buffer.append(&mut GEN(97, 122));
+    let letters =
+        vec![GEN(65, 90), GEN(97, 122)]
+            .iter_mut()
+            .fold(vec![], |mut acc, x| {acc.append(x); acc});
 
-    let letters = letters_buffer;
-    let symbols = symbols_buffer;
-    let numbers = GEN(48, 57);
+    let numbers = GEN(48, 57);    // include all single numbers
 
-    let mut demands = env::args()
+    let demands = env::args()
                           .skip(1)
                           .map(|arg| arg.parse::<usize>().expect("Should be positive"))
-                          .collect::<Vec<usize>>();
+                          .collect::<Vec<usize>>(); // get the user's input and parse them into numbers which should be positive
 
     if !demands.is_empty() {
-        if demands[0] >= demands[1] + demands[2] {
+        if demands[0] >= demands[1] + demands[2] { // check if the numbers is legal in logic
             length = demands[0];
             symbol = demands[1];
             number = demands[2];
-            let gen_pwd = |l: usize, s: usize, n: usize| {
+            let gen_pwd = |l, s, n| {
+                // l: amount of letters [A-Z,a-z], say length - symbol - number
+                // s: amount of symbols, say symbol
+                // n: amount of numbers [0-9], say number
 
                 let mut rng = rand::thread_rng();
-
                 let mut password =
                     vec![
                         (l, letters.len(), letters),
@@ -62,18 +62,21 @@ fn main() {
                         ]
                         .iter()
                         .map(|args| {
-                            RAND_IDX(args.0, args.1)
+                            RAND_IDX(args.0, args.1) // generate the random index on corresponding Vec depend on its amount
                                     .iter()
-                                    .map(|idx| args.2[*idx].clone())
-                                    .collect::<Vec<String>>()
+                                    .map(|idx| args.2[*idx].clone())// index their values in to Vec<String>
+                                    .collect()
                             })
                         .fold(vec![], |mut acc, mut x| {acc.append(&mut x);acc});
-                password.shuffle(&mut rng);
-                println!("{:?}", password.join(""));
+                        // unfold these Vec<Vec<String>> in to Vec<String>
+                password.shuffle(&mut rng); // shuffle it
+                println!("{:?}", password.join("")); // join these items into &str
             };
             gen_pwd(length-symbol-number, symbol, number);
-        } else{
+        } else {
             println!("Invalid input!!");
         }
+    } else {
+        println!("Should receive three arguments!");
     }
 }
