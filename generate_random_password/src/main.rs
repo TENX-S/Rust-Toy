045c77a4391a6ac5e798e3bd1c09a01b09f58d71
TENX-S <ttenx@pm.me>
2020-06-24 15:32:34 +0800

    commit

--- generate_random_password/src/main.rs
+++ generate_random_password/src/main.rs
@@ -11,20 +11,6 @@ const GEN: fn(u8, u8) -> Vec<String> = |start, end| {
                      .collect::<Vec<String>>()
 };
 
-const CHECK: fn(&mut Vec<usize>) -> bool = |v| {
-
-    let (length, symbol, number) = (v[0], v[1], v[2]);
-
-    return
-        symbol + number > length
-            &&
-        vec![symbol, number, length]
-            .iter()
-            .filter(|x| **x < 0)
-            .collect::<Vec<&usize>>()
-            .is_empty()
-};
-
 const RAND_IDX: fn(usize, usize) -> Vec<usize> = |n, cnt| {
     let mut rng = rand::thread_rng();
     let mut idx;
@@ -56,38 +42,38 @@ fn main() {
 
     let mut demands = env::args()
                           .skip(1)
-                          .map(|arg| arg.parse::<usize>().unwrap())
+                          .map(|arg| arg.parse::<usize>().expect("Should be positive"))
                           .collect::<Vec<usize>>();
+
     if !demands.is_empty() {
-        if CHECK(&mut demands) {
+        if demands[0] >= demands[1] + demands[2] {
             length = demands[0];
             symbol = demands[1];
             number = demands[2];
+            let gen_pwd = |l: usize, s: usize, n: usize| {
+
+                let mut rng = rand::thread_rng();
+
+                let mut password =
+                    vec![
+                        (l, letters.len(), letters),
+                        (s, symbols.len(), symbols),
+                        (n, numbers.len(), numbers),
+                        ]
+                        .iter()
+                        .map(|args| {
+                            RAND_IDX(args.0, args.1)
+                                    .iter()
+                                    .map(|idx| args.2[*idx].clone())
+                                    .collect::<Vec<String>>()
+                            })
+                        .fold(vec![], |mut acc, mut x| {acc.append(&mut x);acc});
+                password.shuffle(&mut rng);
+                println!("{:?}", password.join(""));
+            };
+            gen_pwd(length-symbol-number, symbol, number);
         } else {
             println!("Invalid input!!");
         }
     }
-
-    let gen_pwd = |l: usize, s: usize, n: usize| {
-
-        let mut rng = rand::thread_rng();
-
-        let mut password =
-            vec![
-                (l, letters.len(), letters),
-                (s, symbols.len(), symbols),
-                (n, numbers.len(), numbers),
-                ]
-                .iter()
-                .map(|args| {
-                    RAND_IDX(args.0, args.1)
-                            .iter()
-                            .map(|idx| args.2[*idx].clone())
-                            .collect::<Vec<String>>()
-                    })
-                .fold(vec![], |mut acc, mut x| {acc.append(&mut x);acc});
-        password.shuffle(&mut rng);
-        println!("{:?}", password.join(""));
-    };
-    gen_pwd(length-symbol-number, symbol, number);
 }
