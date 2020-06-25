7d6dcb9910d5e96b4807b2df23ee778a2842c2a3
TENX-S <ttenx@pm.me>
2020-06-25 10:51:17 +0800

    improve the format

--- generate_random_password/src/main.rs
+++ generate_random_password/src/main.rs
@@ -7,16 +7,17 @@ fn main() {
     let symbol; // amount of the symbols
     let number; // amount of the numbers
 
-    let demands = env::args()
-        .skip(1)
-        .map(|arg| arg.parse::<usize>().expect("Should be positive"))
-        .collect::<Vec<usize>>(); // get the user's input and parse them into numbers which should be positive
+    let requirement =
+        env::args()
+            .skip(1)
+            .map(|arg| arg.parse::<usize>().expect("Should be positive"))
+            .collect::<Vec<usize>>(); // get the user's input and parse them into numbers which should be positive
 
-    if !demands.is_empty() {
-        if demands[0] >= demands[1] + demands[2] { // check if the numbers is legal or not in logic
-            length = demands[0];
-            symbol = demands[1];
-            number = demands[2];
+    if !requirement.is_empty() {
+        if requirement[0] >= requirement[1] + requirement[2] { // check if the numbers is legal or not in logic
+            length = requirement[0];
+            symbol = requirement[1];
+            number = requirement[2];
             let password = GEN_PWD(length-symbol-number, symbol, number);
             println!("{}", password);
         } else { println!("Invalid input!!"); }
