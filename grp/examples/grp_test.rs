07218129b4e1265fecd3bc81e2dc0bd58cc936af
TENX-S <ttenx@pm.me>
2020-07-27 19:25:26 +0800

    support huge numbers

--- grp/examples/grp_test.rs
+++ grp/examples/grp_test.rs
@@ -1,20 +1,104 @@
-use std::env;
-use grp::RandomPassword;
+#![allow(unused)]
+
+extern crate dirs;
+extern crate sys_info;
+
+use grp::*;
+use chrono::prelude::*;
+use std::{
+    io::prelude::*,
+    env, path::Path,
+    fs::{ File, OpenOptions }
+};
 
 
 
-fn main() {
+fn main()
+{
 
     let requirement = env::args()
-        .skip(1)
-        .map(|arg| arg.parse::<usize>().expect("Should be positive"))
-        .collect::<Vec<usize>>(); // get the user's input and parse them into numbers which should be positive
+                                      .skip(1)
+                                      .map(|arg| arg.parse::<BigUint>().expect("Should be positive"))
+                                      .collect::<Vec<_>>(); // get the user's input and parse them into numbers which should be positive
 
-    if !requirement.is_empty() {
-        let (length, sbl_cnt, num_cnt) = (requirement[0], requirement[1], requirement[2]);
-        if length >= sbl_cnt + num_cnt { // check if the numbers is legal or not in logic
-            let r_p = RandomPassword::new(length, sbl_cnt, num_cnt).show();
-            println!("{}", r_p);
-        } else { println!("Invalid input!!"); }
-    } else { println!("{}", RandomPassword::new(12, 1, 3).show()); } // Default
+    if !requirement.is_empty()
+    {
+
+        let (length,          sbl_cnt,         num_cnt)
+            =
+            (requirement[0].clone(), requirement[1].clone(), requirement[2].clone());
+
+        if length >= sbl_cnt.clone() + num_cnt.clone()
+        {
+            save_to_desktop(&RandomPassword::new(length, sbl_cnt, num_cnt).show());
+        }
+
+        else
+        {
+            println!("Invalid input!!");
+        }
+
+    }
+
+    else // Default
+    {
+        println!("{}", RandomPassword::new(12, 1, 3).show());
+    }
+
+}
+
+
+fn save_to_desktop(rp: &str) -> std::io::Result<()>
+{
+
+    let home = dirs::desktop_dir().unwrap();
+    let mut filepath = String::new();
+
+    match sys_info::os_type().unwrap().as_str()
+    {
+        "Darwin" | "Linux" => { filepath = format!("{}/random_password.txt", home.to_str().unwrap()); },
+
+        "Windows" => { filepath = format!("{}\\random_password.txt", home.to_str().unwrap()); },
+
+        _ => ()
+    }
+
+    // if cfg!(target_os = "macos") {
+    //     filepath = format!("{}/random_password.txt", home.to_str().unwrap());
+    // } else if cfg!(target_os = "windows") {
+    //     filepath = format!("{}\\random_password.txt", home.to_str().unwrap());
+    // }
+
+    let mut file: File;
+
+    if !Path::new(filepath.as_str()).exists()
+        &&
+        Path::new(filepath.as_str()).is_file()
+    {
+        file = File::create(filepath.as_str())?;
+    }
+
+    file = OpenOptions::new()
+                       .append(true)
+                       .open(filepath.as_str())?;
+
+    writeln!(&mut file, "{}\n{}\n", now_time(), rp)?;
+
+    Ok(())
+
+}
+
+
+#[inline]
+fn now_time() -> String {
+
+    Local::now()
+          .time()
+          .to_string()
+          .chars()
+          .into_iter()
+          .map(|c| c.to_string())
+          .collect::<Vec<_>>()[..8]
+          .join("")
+
 }
--- grp/src/lib.rs
+++ grp/src/lib.rs
@@ -1,30 +1,69 @@
 #![allow(non_snake_case)]
-use rand::prelude::*;
 
-pub struct RandomPassword {
-    length: usize,
-    sbl_cnt: usize,
-    num_cnt: usize,
+use rand::prelude::*;
+pub use num_bigint::{ BigUint, ToBigUint };
+
+pub struct RandomPassword
+{
+
+    length: BigUint,
+    sbl_cnt: BigUint,
+    num_cnt: BigUint,
     content: String,
+
 }
 
-impl RandomPassword {
 
-    pub fn new(length: usize, sbl_cnt: usize, num_cnt: usize) -> Self {
-        RandomPassword { length, sbl_cnt, num_cnt, content: String::from("") }
+impl RandomPassword
+{
+
+    /// Return an instance of RandomPassword
+    pub fn new(length: impl ToBigUint, sbl_cnt: impl ToBigUint, num_cnt: impl ToBigUint) -> Self
+
+    {
+
+        RandomPassword {
+
+            length: length.to_biguint().unwrap(),
+            sbl_cnt: sbl_cnt.to_biguint().unwrap(),
+            num_cnt: num_cnt.to_biguint().unwrap(),
+            content: String::new(),
+
+        }
+
     }
 
-    pub fn show(&mut self) -> String {
+
+    /// Return the string of random password
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use crate::grp::*;
+    /// let mut rp = RandomPassword::new(10, 2, 3);
+    /// println!("{}", rp.show());
+    /// ```
+    ///
+    pub fn show(&mut self) -> String
+    {
+
         let mut rng = rand::thread_rng();
-        let data = _DATA();
-        let mut PWD = vec![(self.length-self.sbl_cnt-self.num_cnt, data.0), (self.sbl_cnt, data.1), (self.num_cnt, data.2)]
+        let data = Self::_DATA();
+        let mut PWD =
+            vec!
+            [
+                (self.length.clone()-self.sbl_cnt.clone()-self.num_cnt.clone(), data.0),
+                (self.sbl_cnt.clone(), data.1),
+                (self.num_cnt.clone(), data.2)
+            ]
             .iter()
-            .map(|args| {
-                Self::_RAND_IDX(args.0, args.1.len()) // generate the random index on corresponding Vec depend on its amount
-                    .iter()
-                    .map(|idx| args.1[*idx].clone())// index their values in to Vec<String>
-                    .collect()
-            })
+            .map(|args|
+                {   // generate the random index on corresponding Vec depend on its amount
+                    Self::_RAND_IDX(args.0.clone(), args.1.len())
+                        .iter()
+                        .map(|idx| args.1[*idx].clone())// index their values in to Vec<String>
+                        .collect()
+                })
             .collect::<Vec<Vec<_>>>()
             .concat();
             // or
@@ -32,43 +71,93 @@ impl RandomPassword {
 
         PWD.shuffle(&mut rng);
         self.content = PWD.join("");
+
         self.content.clone()
+
     }
 
+
+    /// Generate n random numbers up to cnt
+    /// # Example
+    ///
+    /// ```
+    /// use crate::grp::*;
+    /// let random_indexs = _RAND_IDX(5, 10);
+    /// println!("{:?}", random_indexs);
+    /// ```
+    ///
     #[inline]
-    fn _RAND_IDX(n: usize, cnt: usize) -> Vec<usize> {
+    fn _RAND_IDX(mut n: BigUint, cnt: usize) -> Vec<usize>
+    {
+
         let mut rng = rand::thread_rng();
         let mut idx;
         let mut idxs = vec![];
-        for _ in 0..n {
+
+        while n != 0.to_biguint().unwrap()
+        {
             idx = rng.gen_range(0, cnt);
             idxs.push(idx);
+            n -= 1.to_biguint().unwrap();
         }
+
         idxs
+
+    }
+
+    /// The character set needed to generate a random password
+    #[inline]
+    fn _GEN(range_list: Vec<(u8, u8)>) -> Vec<String>
+    {
+
+        let mut all = vec![];
+
+        for (start, end) in range_list
+        {
+            let mut v = (start..=end).collect::<Vec<_>>()
+                                                .iter()
+                                                .map(|asc_num| (*asc_num as char).to_string())
+                                                .collect();
+            all.append(&mut v);
+        }
+
+        all
+
+    }
+
+    fn _DATA() -> (Vec<String>, Vec<String>, Vec<String>)
+    {
+
+        let letters: Vec<String> = Self::_GEN(vec![(65, 90), (97, 122)]);
+        let symbols: Vec<String> = Self::_GEN(vec![(33, 47), (58, 64), (91, 96), (123, 126)]);
+        let numbers: Vec<String> = Self::_GEN(vec![(48, 57)]);
+
+        (letters, symbols, numbers)
+
     }
 }
 
-const _GEN: fn(Vec<(u8, u8)>) -> Vec<String> = |range_list| {
-    let mut all = vec![];
-    for (start, end) in range_list {
-        let mut v = (start..=end)
-            .collect::<Vec<u8>>()
-            .iter()
-            .map(|asc_num| (*asc_num as char).to_string())
-            .collect();
-        all.append(&mut v);
-    }
-    all
-};
+// const _GEN: fn(Vec<(u8, u8)>) -> Vec<String> = |range_list| {
+//     let mut all = vec![];
+//     for (start, end) in range_list {
+//         let mut v = (start..=end)
+//             .collect::<Vec<_>>()
+//             .iter()
+//             .map(|asc_num| (*asc_num as char).to_string())
+//             .collect();
+//         all.append(&mut v);
+//     }
+//     all
+// };
 
 
-const _DATA: fn() -> (Vec<String>, Vec<String>, Vec<String>) = || {
-    let letters: Vec<String> = _GEN(vec![(65, 90), (97, 122)]);
-    let symbols: Vec<String> = _GEN(vec![(33, 47), (58, 64), (91, 96), (123, 126)]);
-    let numbers: Vec<String> = _GEN(vec![(48, 57)]);
+// const _DATA: fn() -> (Vec<String>, Vec<String>, Vec<String>) = || {
+//     let letters: Vec<String> = _GEN(vec![(65, 90), (97, 122)]);
+//     let symbols: Vec<String> = _GEN(vec![(33, 47), (58, 64), (91, 96), (123, 126)]);
+//     let numbers: Vec<String> = _GEN(vec![(48, 57)]);
 
-    (letters, symbols, numbers)
-};
+//     (letters, symbols, numbers)
+// };
 
 
 // const _RAND_IDX: fn(usize, usize) -> Vec<usize> = |n, cnt| { // Given the amount and upper bound, generate the random index
@@ -82,24 +171,34 @@ const _DATA: fn() -> (Vec<String>, Vec<String>, Vec<String>) = || {
 //     idxs
 // };
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn _GEN_works() {
-        assert_eq!(_GEN(vec![(48, 57)]), vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-        assert_eq!(_GEN(vec![(33, 47), (58, 64), (91, 96), (123, 126)]), vec!["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]);
-        assert_eq!(_GEN(vec![(65, 90), (97, 122)]), vec!["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+    fn _GEN_works()
+    {
+
+        assert_eq!(RandomPassword::_GEN(vec![(48, 57)]), vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+        assert_eq!(RandomPassword::_GEN(vec![(33, 47), (58, 64), (91, 96), (123, 126)]), vec!["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]);
+        assert_eq!(RandomPassword::_GEN(vec![(65, 90), (97, 122)]), vec!["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+
     }
 
+
     #[test]
-    fn _RAND_IDX_works() {
-        let ret = _RAND_IDX(10_000, 100_0000)
-            .iter()
-            .filter(|x| **x > 100_0000)
-            .collect::<Vec<&usize>>()
-            .is_empty();
+    fn _RAND_IDX_works()
+    {
+
+        let ret = RandomPassword::_RAND_IDX(10_000.to_biguint().unwrap(), 100_0000)
+                                      .iter()
+                                      .filter(|x| **x > 100_0000)
+                                      .collect::<Vec<&usize>>()
+                                      .is_empty();
+
         assert!(ret);
+
     }
+
 }
