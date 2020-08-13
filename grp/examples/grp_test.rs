f7d58719c44f4ad346fe1397369ec74c4c5a5a44
TENX-S <ttenx@pm.me>
2020-08-13 18:08:27 +0800

    simplify the code

--- grp/examples/grp_test.rs
+++ grp/examples/grp_test.rs
@@ -3,8 +3,6 @@ use std::env;
 use num_bigint::BigUint;
 
 
-
-
 fn main() {
 
     let demands = env::args().skip(1).map(|arg| arg.parse::<BigUint>().unwrap()).collect::<Vec<_>>();
@@ -12,18 +10,17 @@ fn main() {
     let mut r_p;
 
     if demands.is_empty() {
-        r_p = RandomPassword::new(10, 2, 3);
-        r_p.mix();
-        println!("\n{}\n", r_p.show());
-    }
-    else {
+        r_p = RandPwd::new(10, 2, 3);
+        r_p.join();
+        println!("\n{}\n", r_p);
+    } else {
         let ltr_cnt = demands[0].clone();
         let sbl_cnt = demands[1].clone();
         let num_cnt = demands[2].clone();
 
-        r_p = RandomPassword::new(ltr_cnt, sbl_cnt, num_cnt);
-        r_p.mix();
-        println!("\n{}\n", r_p.show());
+        r_p = RandPwd::new(ltr_cnt, sbl_cnt, num_cnt);
+        r_p.join();
+        println!("\n{}\n", r_p);
     }
 
 }
--- grp/src/lib.rs
+++ grp/src/lib.rs
@@ -3,15 +3,15 @@
 
 use rand::prelude::*;
 use rayon::prelude::*;
-use std::ops::SubAssign;
 use num_bigint::{ BigUint, ToBigUint };
 use num_traits::{ Zero, One, ToPrimitive };
+use std::{ fmt::{ Display, Formatter, Result }, ops::SubAssign };
 
 
 
-/// struct `RandomPassword`
+/// struct `RandPwd`
 #[derive(Clone, Debug)]
-pub struct RandomPassword {
+pub struct RandPwd {
     ltr_cnt: BigUint,
     sbl_cnt: BigUint,
     num_cnt: BigUint,
@@ -19,20 +19,21 @@ pub struct RandomPassword {
     _UNIT: usize,
 }
 
-
+/// Type alias for the parameter of method `_PWD`,
+/// `T` represents the count of characters should be used,
+/// `&[String]` represent the corresponding characters set
 type I<'a, T> = (&'a T, &'a [String]);
 
-pub trait TBU = ToBigUint;
-trait P = TBU + Clone + SubAssign + PartialOrd;
+trait P = ToBigUint + Clone + SubAssign + PartialOrd;
 
-impl RandomPassword {
+impl RandPwd {
 
-    /// Return an empty instance of `Result<RandomPassword, &'static str>`
+    /// Return an empty instance of `Result<RandPwd, &'static str>`
     /// # Example
     /// ```
-    /// use grp::RandomPassword;
+    /// use grp::RandPwd;
     /// use num_bigint::BigUint;
-    /// let mut r_p = RandomPassword::new(11, 4, 2);
+    /// let mut r_p = RandPwd::new(11, 4, 2);
     ///
     /// // If you want push a large number in it
     /// // parse the `&str` into `BigUint`
@@ -42,12 +43,12 @@ impl RandomPassword {
     /// let sbl_cnt = BigUint::from_str(&format!("{}000", usize::MAX)).unwrap();
     /// let num_cnt = BigUint::from_str(&format!("{}000", usize::MAX)).unwrap();
     ///
-    /// r_p = RandomPassword::new(ltr_cnt, sbl_cnt, num_cnt);
+    /// r_p = RandPwd::new(ltr_cnt, sbl_cnt, num_cnt);
     /// ```
     #[inline]
-    pub fn new<T: TBU>(ltr_cnt: T, sbl_cnt: T, num_cnt: T) -> Self {
+    pub fn new<T: ToBigUint>(ltr_cnt: T, sbl_cnt: T, num_cnt: T) -> Self {
 
-        RandomPassword {
+        RandPwd {
             ltr_cnt: ltr_cnt.to_biguint().unwrap(),
             sbl_cnt: sbl_cnt.to_biguint().unwrap(),
             num_cnt: num_cnt.to_biguint().unwrap(),
@@ -60,65 +61,92 @@ impl RandomPassword {
 
     /// Generate the password
     #[inline]
-    pub fn mix(&mut self) {
-        let data = Self::_DATA();
-        let mut PWD: String = self._PWD((&self.ltr_cnt, &data.0),
-                                        (&self.sbl_cnt, &data.1),
-                                        (&self.num_cnt, &data.2),);
+    pub fn join(&mut self) {
+        let data = Self::_DATA(); // TODO : - Cached this variable
+        let mut PWD: String = self._PWD((&self.ltr_cnt, &data[0]),
+                                        (&self.sbl_cnt, &data[1]),
+                                        (&self.num_cnt, &data[2]),);
         let bytes = unsafe { PWD.as_bytes_mut() };
         bytes.shuffle(&mut thread_rng());
         self.content = bytes.par_iter().map(|s| *s as char).collect::<String>();
     }
 
-    /// Return the string of random password
+    /// Return the content of random password in `&str`
     /// # Example
     ///
     /// ```
-    /// use grp::RandomPassword;
-    /// let mut rp = RandomPassword::new(10, 2, 3);
+    /// use grp::RandPwd;
+    /// let mut rp = RandPwd::new(10, 2, 3);
     /// rp.mix();
     /// println!("{}", rp.show());
     /// // Output: 0fajn-ulS8S}7sn
     /// ```
     #[inline]
-    pub fn show(&mut self) -> &str { &self.content }
+    pub fn show(&self) -> &str {
+        &self.content
+    }
 
-    /// Returns the length of this `RandomPassword`, in both bytes and [char]s.
-    #[inline]
-    pub fn len(&self) -> usize { self.content.len() }
 
-    /// Returns true if this `RandomPassword` has a length of zero, and false otherwise.
+    /// Returns the length of this `RandPwd`, in both bytes and [char]s.
     #[inline]
-    pub fn is_empty(&self) -> bool { self.content.is_empty() }
+    pub fn len(&self) -> usize {
+        self.content.len()
+    }
+
+
+    /// Returns true if this `RandPwd` has a length of zero, and false otherwise.
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.content.is_empty()
+    }
+
 
     /// The value of UNIT is inversely proportional to memory overhead
-    /// In order to increase CPU time and reduce the memory overhead, raise the value of `UNIT`
+    /// In order to reduce the memory overhead, raise the value of `UNIT`
     #[inline]
-    pub fn set_unit(&mut self, val: usize) { self._UNIT = val; }
-
-    /// Change the letter's count of `RandomPassword`
-    #[inline]
-    pub fn set_ltr_cnt<T: TBU>(&mut self, val: T) {
-        self.ltr_cnt = val.to_biguint().unwrap();
+    pub fn set_unit(&mut self, val: usize) {
+        self._UNIT = val;
     }
 
-    /// Change the symbol's count of `RandomPassword`
+    /// Change the count of letters, symbols or numbers of `RandPwd`
+    /// ```
+    /// use grp::*;
+    /// let r_p = RandPwd::new(10, 2, 3);
+    ///
+    /// // Set the letter's count
+    /// r_p.set_cnt("ltr", 0);
+    /// r_p.join();
+    /// println!("{}", r_p.show());
+    /// // Output: *029(
+    ///
+    /// // Set the symbol's count
+    /// r_p.set_cnt("sbl", 0);
+    /// r_p.join();
+    /// println!("{}", r_p.show());
+    /// // Output: nz1MriAl0j5on
+    ///
+    /// // Set the number's count
+    /// r_p.set_cnt("num", 0);
+    /// r_p.join();
+    /// println!("{}", r_p.show());
+    /// // Output: +iQiQGSXl(nv
+    /// ```
     #[inline]
-    pub fn set_sbl_cnt<T: TBU>(&mut self, val: T) {
-        self.sbl_cnt = val.to_biguint().unwrap();
-    }
+    pub fn set_cnt<T: ToBigUint>(&mut self, kind: &str, val: T) {
+        match kind {
 
-    /// Change the number's count of `RandomPassword`
-    #[inline]
-    pub fn set_num_cnt<T: TBU>(&mut self, val: T) {
-        self.num_cnt = val.to_biguint().unwrap();
+            "ltr" => self.ltr_cnt = val.to_biguint().unwrap(),
+            "sbl" => self.sbl_cnt = val.to_biguint().unwrap(),
+            "num" => self.num_cnt = val.to_biguint().unwrap(),
+
+            _     => (),
+        }
     }
 
     /// Generate random password
     #[inline]
-    pub(crate) fn _PWD<'a, T: P>(&self, letters: I<'a, T>, symbols: I<'a, T>, numbers: I<'a, T>) -> String
-    {
-
+    pub(crate) fn _PWD<'a, T: P>(&self, letters: I<'a, T>, symbols: I<'a, T>, numbers: I<'a, T>) -> String {
+        // TODO: - Improve readability
         vec![(letters.0, letters.1),
              (symbols.0, symbols.1),
              (numbers.0, numbers.1),]
@@ -129,6 +157,7 @@ impl RandomPassword {
                     .map(|cnt| {
                         Self::_RAND_IDX(*cnt, data.len())
                             .par_iter()
+                            // TODO : - Remove this clone which can cause huge overhead of both memory and CPU
                             .map(|idx| data[*idx].clone())
                             .collect::<String>()
                     })
@@ -140,10 +169,10 @@ impl RandomPassword {
 
     }
 
-    /// Decompose large numbers into smaller numbers
+
+    /// Resolve large numbers into smaller numbers
     #[inline]
-    pub(crate) fn _DIV_UNIT<T: P>(&self, n: &T) -> Vec<usize>
-    {
+    pub(crate) fn _DIV_UNIT<T: P>(&self, n: &T) -> Vec<usize> {
 
         let mut n = n.to_biguint().unwrap();
 
@@ -167,7 +196,7 @@ impl RandomPassword {
 
     /// Generate n random numbers, each one is up to cnt
     #[inline]
-    pub(crate) fn _RAND_IDX(n: impl TBU, cnt: usize) -> Vec<usize> {
+    pub(crate) fn _RAND_IDX(n: impl ToBigUint, cnt: usize) -> Vec<usize> {
 
         let mut n = n.to_biguint().unwrap();
         let mut idxs = Vec::with_capacity(n.to_usize().unwrap());
@@ -181,40 +210,47 @@ impl RandomPassword {
 
     }
 
-    /// The character set needed to generate a random password
-    #[inline]
-    pub(crate) fn _GEN(range_list: Vec<(u8, u8)>) -> Vec<String> {
 
-        range_list
-            .into_iter()
-            .map(|(start, end)|
-                (start..=end)
-                    .collect::<Vec<_>>()
-                    .into_iter()
-                    .map(|asc_num|
-                        (asc_num as char).to_string()
-                    )
-                    .collect::<Vec<_>>()
-            )
+    /// Characters set
+    /// return letters, symbols, numbers in `Vec<Vec<String>>`
+    #[inline]
+    pub(crate) fn _DATA() -> Vec<Vec<String>> {
+
+        let GEN = |range_list: &[(u8, u8)]|
+            range_list
+                .into_iter()
+                .map(|(start, end)|
+                    (*start..=*end)
+                        .collect::<Vec<_>>()
+                        .into_iter()
+                        .map(|asc_num|
+                            (asc_num as char).to_string()
+                        )
+                        .collect::<Vec<_>>()
+                )
+                .collect::<Vec<_>>()
+                .concat();
+
+        vec![
+            &[(65, 90), (97, 122)][..],                      // letters
+            &[(33, 47), (58, 64), (91, 96), (123, 126)][..], // symbols
+            &[(48, 57)][..],                                 // numbers
+        ]
+            .iter()
+            .map(|x| GEN(&x[..]))
             .collect::<Vec<_>>()
-            .concat()
-
-    }
-
-    /// Range of character set
-    /// return (letters, symbols, numbers)
-    #[inline]
-    pub(crate) fn _DATA() -> (Vec<String>, Vec<String>, Vec<String>) {
-
-        (
-            Self::_GEN(vec![(65, 90), (97, 122)]),
-            Self::_GEN(vec![(33, 47), (58, 64), (91, 96), (123, 126)]),
-            Self::_GEN(vec![(48, 57)])
-        )
 
     }
 
 }
+
+impl Display for RandPwd {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}", self.show())
+    }
+}
+
+
 
 
 #[cfg(test)]
@@ -222,18 +258,20 @@ mod tests {
 
     use super::*;
 
+
     #[test]
     fn _GEN_works() {
 
-        assert_eq!(RandomPassword::_GEN(vec![(48, 57)]), vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-        assert_eq!(RandomPassword::_GEN(vec![(33, 47), (58, 64), (91, 96), (123, 126)]), vec!["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]);
-        assert_eq!(RandomPassword::_GEN(vec![(65, 90), (97, 122)]), vec!["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+        assert_eq!(RandPwd::_GEN(&vec![(48, 57)]), vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+        assert_eq!(RandPwd::_GEN(&vec![(33, 47), (58, 64), (91, 96), (123, 126)]), vec!["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]);
+        assert_eq!(RandPwd::_GEN(&vec![(65, 90), (97, 122)]), vec!["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
 
     }
 
+
     #[test]
     fn _RAND_IDX_works() {
-        assert!(RandomPassword::_RAND_IDX(10_000.to_biguint().unwrap(), 100_0000)
+        assert!(RandPwd::_RAND_IDX(10_000.to_biguint().unwrap(), 100_0000)
                                .into_iter()
                                .filter(|x| *x > 100_0000)
                                .collect::<Vec<_>>()
