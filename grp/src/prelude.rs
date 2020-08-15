a34fae2d687cef71fff4edb1fa04de20490a6319
TENX-S <ttenx@pm.me>
2020-08-15 17:07:58 +0800

    impl Default

--- grp/src/prelude.rs
+++ grp/src/prelude.rs
@@ -13,40 +13,40 @@ pub use std::{ fmt::{ Display, Formatter, Result }, ops::SubAssign };
 /// `&[String]` represent the corresponding characters set
 pub type I<'a, T> = (&'a T, &'a [String]);
 
-pub type NumSet = heapless::Vec<u8, U26>;
-pub type StrSet = heapless::Vec<String, U52>;
-pub type CharSet = heapless::Vec<StrSet, U3>;
+pub type NumVec = heapless::Vec<u8, U26>;
+pub type StrVec = heapless::Vec<String, U52>;
+pub type CharVec = heapless::Vec<StrVec, U3>;
 
 pub trait P = ToBigUint + Clone + SubAssign + PartialOrd;
 
 
 lazy_static! {
     /// Cached the characters set
-    pub static ref DATA: CharSet = _DATA();
+    pub static ref DATA: CharVec = _DATA();
 }
 
 /// Characters set
-/// return letters, symbols, numbers in `CharSet`
-pub(crate) fn _DATA() -> CharSet {
+/// return letters, symbols, numbers in `CharVec`
+pub(crate) fn _DATA() -> CharVec {
     let GEN = |range_list: &[(u8, u8)]|
         range_list
             .into_iter()
             .map(|(start, end)|
                 (*start..=*end)
-                    .collect::<NumSet>()
+                    .collect::<NumVec>()
                     .into_iter()
                     .map(|asc_num|
                         (asc_num as char).to_string()
                     )
-                    .collect::<StrSet>()
+                    .collect::<StrVec>()
             )
-            .fold(StrSet::new(), |mut acc, x| { acc.extend_from_slice(&x).unwrap(); acc });
+            .fold(StrVec::new(), |mut acc, x| { acc.extend_from_slice(&x).unwrap(); acc });
 
     [&[(65, 90), (97, 122)][..],
         &[(33, 47), (58, 64), (91, 96), (123, 126)][..],
         &[(48, 57)][..],]
         .iter()
         .map(|x| GEN(x))
-        .collect::<CharSet>()
+        .collect::<CharVec>()
 
 }
